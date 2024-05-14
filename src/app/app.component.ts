cb07bdd1f0c40bec236d8ec9768d836cb0256e33
Ruslan <axelezgames@gmail.com>
2024-05-14 23:00:36 +0500

    feat(api): add api fetch usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { interval } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+import { TimestampService } from './utils/getCurrentTimestamp';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,5 +14,34 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'yadro';
+  rates: any;
+  previousRates: any;
+  data: any;
+
+  constructor(private http: HttpClient, private timestampService: TimestampService) {
+    this.getRates();
+    interval(60000).subscribe(() => {
+      this.previousRates = { ...this.rates };
+      console.log(this.previousRates);
+      this.getRates();
+    });
+  }
+
+  getRates() {
+    if (this.rates) this.previousRates = { ...this.rates };
+    console.log(this.previousRates);
+
+    this.http
+      .get<any>('https://open.er-api.com/v6/latest/RUB')
+      .subscribe((data) => {
+        console.log(data);
+
+        this.rates = data.rates;
+        this.data = data;
+      });
+  }
+
+  getCurrentTimestamp(): string {
+    return this.timestampService.getCurrentTimestamp()
+  }
 }
